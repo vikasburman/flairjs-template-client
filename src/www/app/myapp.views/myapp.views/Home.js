909b3f33770e8abf049cab17818f66e9ee347118
Vikas Burman <vikasburman@gmail.com>
2019-09-10 01:30:22 -0400

    ...

--- src/www/app/myapp.views/myapp.views/Home.js
+++ src/www/app/myapp.views/myapp.views/Home.js
@@ -6,10 +6,6 @@ const ClientDateTime = await include('myapp.services.ClientDateTime');
  * @description Default Home View
  */
 Class('', VueView, function() {
-    this.i18n = 'titles, strings';
-    this.title = '@titles.home | Home';
-    this.html = './views/home.html';
-
     $$('override');
     this.loadData = async (base, ctx) => { // eslint-disable-line no-unused-vars
         base(ctx);
