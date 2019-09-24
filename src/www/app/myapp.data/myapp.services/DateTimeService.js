eb5138874427aa0fc99093caf3e9965b6520af15
Vikas Burman <vikasburman@gmail.com>
2019-09-24 12:38:44 -0400

    ...

--- src/www/app/myapp.data/myapp.services/DateTimeService.js
+++ src/www/app/myapp.data/myapp.services/DateTimeService.js
@@ -5,17 +5,27 @@
 $$('static');
 Class('', function() {
     $$('cache', 10000);
-    this.now = async () => {
+    this.now = async (type = 'ms') => {
         // pure client way of keeping data logic separate 
         // on an app that connects to server it can fetch data from server
         // as shown below
-        return Date.now();
+        let result = '';
+        switch (type) {
+            case 'ms': result = Date.now().toString(); break;
+            case 'iso': result = new Date().toISOString(); break;
+            case 'utc': result = new Date().toUTCString(); break;
+            default: result = new Date().toJSON().toString(); break;
+        }
+        return result;
     };
 
     // $$('cache', 10000);
-    // $$('fetch', 'app-server', 'get-json', '/now');
-    // this.now = async (api) => {
-    //     let result = await api();
+    // $$('fetch', 'app-server', 'get-json', '/now/:format');
+    // this.now = async (api, format = 'ms') => {
+    //     let apiArgs = {
+    //         format: format
+    //     };
+    //     let result = await api(apiArgs);
     //     return result || 'Could not connect to server.';
     // };
 });
